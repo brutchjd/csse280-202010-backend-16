0749d8eef54748831e5fc9e794305985e8de52b6
Mohammed A Ali <alima@rose-hulman.edu>
2019-11-13 14:07:34 -0500

    tried to fix some syntax for back end deployment

--- models/db.js
+++ models/db.js
@@ -3,7 +3,7 @@ const server = '127.0.0.1:27017';
 const database = 'oraclecarsdb';
 
 let dbURI = `mongodb://${server}/${database}`;
-if(process.env.NODE_ENV === "production") {
+if(process.env.NODE_ENV === 'production') {
     dbURI = process.env.MONGODB_ATLAS_URI;
 }
 
